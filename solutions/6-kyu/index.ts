fb0e2ae37d3ad406b9aaa24f8556282647ae8f24
mirzabekov00 <prog.eldar@gmail.com>
2021-10-19 21:24:43 +0300

    feat(6-kyu): resolve `Sums of Parts` kata

--- solutions/6-kyu/index.ts
+++ solutions/6-kyu/index.ts
@@ -1,5 +1,11 @@
 import { findEvenIndex } from "./Equal Sides Of An Array";
 import { getLengthOfMissingArray } from "./Length of missing array";
 import { multiplicationTable } from "./Multiplication table";
+import { partsSums } from "./Sums of Parts";
 
-export { findEvenIndex, getLengthOfMissingArray, multiplicationTable };
+export {
+  findEvenIndex,
+  getLengthOfMissingArray,
+  multiplicationTable,
+  partsSums,
+};
