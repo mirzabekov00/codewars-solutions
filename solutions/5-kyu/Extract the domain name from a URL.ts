69423db176f0fb0e19b51a47529168acbf8ea85c
mirzabekov00 <prog.eldar@gmail.com>
2021-10-26 21:30:47 +0300

    feat(task): add description

--- solutions/5-kyu/Extract the domain name from a URL.ts
+++ solutions/5-kyu/Extract the domain name from a URL.ts
@@ -1,6 +1,9 @@
 /**
  * Description:
- *
+ * Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+   domainName("http://github.com/carbonfive/raygun") == "github" 
+   domainName("http://www.zombie-bites.com") == "zombie-bites"
+   domainName("https://www.cnet.com") == "cnet"
  */
 export const domainName = (url: string): string => {
   return url
